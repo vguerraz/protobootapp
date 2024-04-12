ba0c878fabb6256911655f0cdc4982d476d3474d
leonj <leonj@192.168.1.4>
2024-04-12 17:49:23 -0500

    Changed a comment

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -1,6 +1,6 @@
 package co.edu.eafit.protobootapp;
 
-//This is a simple calculator class.
+//This is a simple calculator class
 public class Calculadora {
 
     public Double cuadrado(Double numero) {
