03ef71cbe82761fc928bf21c084adf97d8b4d91e
León Jaramillo <leonjaramillo@gmail.com>
2025-03-15 09:58:10 -0500

    Update Calculadora.java with a new message

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -1,6 +1,6 @@
 package co.edu.eafit.protobootapp;
 
-//This is a simple calculator class
+//This is a simple calculator class and I-m editing it
 public class Calculadora {
 
     public Double cuadrado(Double numero) {
